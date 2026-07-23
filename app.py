482f6e6f31969ab0e74ca9de095ab5cf69089d5d
vsPace <vstakr@gmail.com>
2026-07-24 01:24:20 +0330

    Add GitHub Actions workflow and Docker runtime test

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from rubpy import Client
 async def main():
     async with Client("time_sessions") as app:
         me = await app.get_me()
+        print("===================== API RESPONSE =================")
         print(me)
+        print("====================================================")
 
 if __name__ == "__main__":
     asyncio.run(main())
