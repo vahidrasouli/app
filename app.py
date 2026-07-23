39f20edf0e37c3705c74c0fdfc6ea999363d6742
vsPace <vstakr@gmail.com>
2026-07-24 00:52:55 +0330

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import asyncio
+from rubpy import Client
+import requests
+
+async def main():
+    async with Client("time_sessions") as app:
+        me = await app.get_me()
+        print(me)
+
+if __name__ == "__main__":
+    asyncio.run(main())
